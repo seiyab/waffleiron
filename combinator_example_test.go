2b513ccea1c6421a20c52b6a38db52d12ae5db98
seiya <r108338@yahoo.co.jp>
2022-02-06 22:57:22 +0900

    add example test

---
+++ combinator_example_test.go
@@ -0,0 +1,24 @@
+package waffleiron_test
+
+import (
+	"fmt"
+
+	wi "github.com/seiyab/waffleiron"
+)
+
+func Example() {
+	p := wi.Between(
+		wi.Rune('['),
+		wi.SepBy(wi.Int, wi.Rune(',')),
+		wi.Rune(']'),
+	)
+	
+	result, err := wi.Parse("[0,1,2,3]", p)
+
+	fmt.Println(err)
+	fmt.Println(fmt.Sprintf("%#v", result))
+
+	// Output:
+	// <nil>
+	// []int{0, 1, 2, 3}
+}
